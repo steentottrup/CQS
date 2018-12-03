aef8523b0c548f0d051d3f23d1d28b5bc444ccd3
Steen F. Tøttrup <sft@creativeminds.dk>
2018-12-03 07:35:11 +0100

    Adding cancellation token to base dispatcher

--- src/CreativeMinds.CQS/Dispatchers/AsyncEventDispatcherBase.cs
+++ src/CreativeMinds.CQS/Dispatchers/AsyncEventDispatcherBase.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 namespace CreativeMinds.CQS.Dispatchers {
 
@@ -14,13 +15,13 @@ namespace CreativeMinds.CQS.Dispatchers {
 			this.logger = logger;
 		}
 
-		public void PublishAsync<TEvent>(TEvent eventMessage) where TEvent : IEvent {
+		public void PublishAsync<TEvent>(TEvent eventMessage, CancellationToken cancellationToken) where TEvent : IEvent {
 			this.logger.LogDebug($"Publishing the {typeof(TEvent).GetType().Name} event.", eventMessage);
 			IEnumerable<IAsyncEventHandler<TEvent>> handlers = this.GetEventHandlers<TEvent>();
 			this.logger.LogDebug($"Found {handlers.Count()} event handlers.", eventMessage);
 			foreach (IAsyncEventHandler<TEvent> handler in handlers) {
 				try {
-					handler.HandleAsync(eventMessage);
+					handler.HandleAsync(eventMessage, cancellationToken);
 				}
 				catch (Exception ex) {
 					this.logger.LogCritical($"An exception was throw when trying to publish a {eventMessage.GetType().Name} event using the {handler.GetType().Name} handler", ex, eventMessage);
